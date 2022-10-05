c6bae0586fabd89698184c2e9f563a219abdf8d3
Shota Kuchiki <zk5zg3az@i.softbank.jp>
2022-10-05 23:08:38 +0900

    Add classes of ServletService and HttpServletReuqestImpt

---
+++ server/src/com.sampledomain.henacat/servletimpl/HttpServletRequestImpt.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+import java.nio.charset.*;
+
+public class HttpServletRequestImpt implements HttpServletRequest {
+  private String method;
+  private String characterEncoding = "ISO-8859-1";
+  private Map<String, String[]> parameterMap;
+
+  @Override
+  public String getMethod() {
+    return this.method;
+  }
+
+  @Override
+  public String[] getParameter(String name) {
+    String[] values = getParameterValues(name);
+    if (values == null) {
+      return null;
+    }
+    String[] decoded = new String[values.length];
+    try {
+      for (int i = 0; i < values.length; i++) {
+        decoded[i] = MyURLDecoder.decode(values[i], this.characterEncoding);
+      }
+    } catch (UnsupportedEncodingException ex) {
+      throw new AssertionError(ex);
+    }
+    return decoded;
+  }
+
+  @Override
+  public void setCharacterEncoding(String env) throws UnsupportedEncodingException {
+    if (!Charset.isSupported(env)) {
+      throw new UnsupportedEncodingException("encoding.." + env);
+    }
+    this.characterEncoding = env;
+  }
+
+  HttpServletReuqestImpl(String method, Map<String, String[]> parameterMap) {
+    this.method = method;
+    this.parameterMap = parameterMap;
+  }
+}
---
+++ server/src/com.sampledomain.henacat/servletimpl/ServletService.java
@@ -0,0 +1,107 @@
+import java.util.*;
+import java.io.*;
+
+public class ServletService {
+  private static HttpServlet createServlet(ServletInfo info) throws Exception {
+    Class<?> clazz = info.webApp.classLoader.loadClass(info.servletClassName);
+    // (type)は型キャスト
+    return (HttpServlet)clazz.newInstance();
+  }
+
+  // queryを受け取って、key=> [value1, value2, ...]の形で返却するmethod
+  private static Map<String, String[]> stringToMap(String str) {
+    Map<String, String[]> parameterMap = new HashMap<String, String[]>();
+
+    if (str != null) {
+      String[] paramArray = str.split("&");
+
+      for (String param : paramArray) {
+        String[] keyValue = param.split("=");
+
+        if (parameterMap.containsKey(keyValue[0])) {
+          // すでに該当のkeyが含まれている場合、
+          // key => [old_value]からkey => [old_value, new_value]のようにvalueの配列末尾に新しいvalueを追加する
+          String[] array = parameterMap.get(keyValue[0]);
+
+          // arrayの末尾に空の要素を追加したnewArrayを作る
+          String[] newArray = new String[array.length + 1];
+          System.arraycopy(array, 0, newArray, 0, array.length);
+
+          newArray[array.length] = keyValue[1];
+          parameterMap.put(keyValue[0], newArray);
+        } else {
+          parameterMap.put(keyValue[0], new String[] {keyValue[1]});
+        }
+      }
+    }
+    return parameterMap;
+  }
+
+  // サイズで指定された行まで読みだすmethod
+  private static String readToSize(InputStream input, int size) throws Exception {
+    int ch;
+    StringBuilder sb = new StringBuilder();
+    int readSize = 0;
+
+    while (readSize < size && (ch = input.read()) != -1 ) {
+      sb.append((char)ch);
+      readSize++;
+    }
+    return sb.toString();
+  }
+
+  public static void doService(
+    String method,
+    String query,
+    ServletInfo info,
+    Map<String, String> requestHeader,
+    InputStream input,
+    OutputStream output
+  ) {
+    if (info.servlet == null) {
+      info.servlet = createServlet(info);
+    }
+
+    ByteArrayOutputStream outputBuffer = new ByteArrayOutputStream();
+    HttpServletResponseImpl resp = new HttpServletResponseImpl(outputBuffer);
+    HttpServletRequest req;
+
+    if (method.equals("GET")) {
+      Map<String, String[]> map;
+      map = stringToMap(query);
+      req = new HttpServletRequestImpl("GET", map);
+    } else if (method.equals("POST")) {
+      int contentLength = Integer.parseInt(requestHeader.get("CONTENT-LENGTH"));
+      Map<String, String[]> map;
+      String line = readToSize(input, contentLength);
+      map = stringToMap(line);
+      req = new HttpServletRequestImpl("POST", map);
+    } else {
+      throw new AssertionError("BAD METHOD:" + method);
+    }
+
+    info.servlet.service(req, resp);
+
+    if (resp.status == HttpServletResponse.SC_OK) {
+      SendResponse.sendOkResponseHeader(output, resp.contentType);
+      resp.printWriter.flush();
+
+      // NOTE: レスポンスボディは順次書き出されるが、先にレスポンスヘッダを返す必要があるので、
+      //       一旦全てバッファリングする。
+      byte[] outputBytes = outputBuffer.toByteArray();
+      for (byte b: outputBytes) {
+        output.write((int)b);
+      }
+    } else if (resp.status == HttpServletResponse.SC_FOUND) {
+      String redirectLocation;
+
+      if (resp.redirectLocation.startsWith("/")) {
+        String host = requestHeader.get("HOST");
+        redirectLocation = "http://" + ((host != null) ? host : Constants.SERVER_NAME) + resp.redirectLocation;
+      } else {
+        redirectLocation = resp.redirectLocation;
+      }
+      SendResponse.sendFoundResponse(output, redirectLocation);
+    }
+  }
+}
