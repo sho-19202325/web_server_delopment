287d618105d003f47d77e992a8d699232dadc27b
Shota Kuchiki <zk5zg3az@i.softbank.jp>
2022-10-09 16:36:01 +0900

    Add jsp files to return html file dynamically

---
+++ server/src/tomcat/testbbs_jsp/WEB-INF/java/bbs/Message.java
@@ -0,0 +1,18 @@
+package bbs;
+import java.util.*;
+
+public class Message {
+  public static ArrayList<Message> messageList = new ArrayList<Message>();
+
+  public String title;
+  public String handle;
+  public String message;
+  public Date date;
+
+  public Message(String title, String handle, String message) {
+    this.title = title;
+    this.handle = handle;
+    this.message = message;
+    this.date = new Date();
+  }
+}
