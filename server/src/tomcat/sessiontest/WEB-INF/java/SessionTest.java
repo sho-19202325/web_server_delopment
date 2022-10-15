40252b0aaea6d660d4468557304324cfee57c81c
Shota Kuchiki <zk5zg3az@i.softbank.jp>
2022-10-15 17:44:49 +0900

    Add SessionTest to tomcat

---
+++ server/src/tomcat/sessiontest/WEB-INF/java/SessionTest.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class SessionTest extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    response.setContentType("text/plain");
+    PrintWriter out = response.getWriter();
+
+    HttpSession session = request.getSession(true);
+    Integer counter = (Integer)session.getAttribute("Counter");
+
+    if (counter == null) {
+      out.println("No session");
+      session.setAttribute("Counter", 1);
+    } else {
+      out.println("Counter.." + counter);
+      session.setAttribute("Counter", counter + 1);
+    }
+  }
+}
